c419e173685bd687e3770efa936575b030ec3a79
shanoof <shanoof.001@gmail.com>
2024-10-29 18:32:36 +0530

    server:deployed json server to render

--- src/utils/API_URL.jsx
+++ src/utils/API_URL.jsx
@@ -1,3 +1,3 @@
-export const userURL = "http://localhost:4000/user";
-export const itemsURL = "http://localhost:4000/items";
-export const bestSellers = "http://localhost:4000/bestsellers";
+export const userURL = "https://kicks-json-server.onrender.com/user";
+export const itemsURL = "https://kicks-json-server.onrender.com/items";
+export const bestSellers = "https://kicks-json-server.onrender.com/bestSellers";
